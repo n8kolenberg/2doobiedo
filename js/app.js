23492ee803369c2a5dcaf1ad6691d430ad36152e
n8kolenberg <nkolenberg@gmail.com>
2015-06-12 00:42:28 +0200

    Add validation to input, bug fixes for click and enter features, basic styling

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 $(document).ready( function() {
 
-//Main Feature of Adding Tasks
+
+//Hide initial warning to add task
+$('.warning').hide();
+
+
+//Use button click to add tasks
 $('.submit-btn').on('click', function (event){
 	event.preventDefault();
-	var newTask = $('.input-field').val()
-	$('#sortable').prepend("<li>"+newTask+"</li>");
-	$('.input-field').val('')
-
+	var $newTask = $('input:text').val();
+	if(!$.trim($newTask)) { 
+		$('.warning').fadeIn(300);
+	} else { 
+		$('.warning').fadeOut(300);
+		$('li:first').before("<li>"+$newTask+"</li>");
+		$('input:text').val('')
+	} //End if statement
 }); //End on click
 
 
@@ -14,26 +23,23 @@ $('.submit-btn').on('click', function (event){
 $('.input-field').on('keydown', function (event) {
 	if(event.which === 13) {
 		event.preventDefault();
-		var newTask = $('.input-field').val();
-		$('#sortable').prepend("<li>"+newTask+"</li>");
-		$('.input-field').val('')
-		
-	}
+		$('.submit-btn').click();
+		$('input:text').val('')
+	} //End if statement
 }); // End on keydown
 
+//Make the Task List Sortable
 $('#sortable').sortable({axis : "y"});
 
-// $('#sortable li').on('click', function(){
-// 	alert($(this).text());
-// });
-var updatedItem;
 
+var updatedItem;
 $('#sortable li').on('dblclick', function (){
 	$(this).toggleClass('listclick');
 	updatedItem = prompt('Update Item');
-	$(this).html("<li>"+updatedItem+"</li>"); //End replaceWith callback
+	$(this).html("<li>"+updatedItem+"</li>");
 
-}); //End sortable
+});// End jQuery selection for updateItem function
+
 
 
 //Selectable
