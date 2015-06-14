ac8582aad9d9e7261b0f31262ec550627b9ad44e
n8kolenberg <nkolenberg@gmail.com>
2015-06-14 19:57:22 +0200

    Add a lot more interactivity: colors, animation when adding tasks, ability to select and delete multiple items simultaneously, ability to show and hide instructions

--- js/app.js
+++ js/app.js
@@ -4,6 +4,19 @@ $(document).ready( function() {
 //Hide initial warning to add task
 $('.warning').hide();
 
+//Make instructions appear and disappear
+$('.intro h3').addClass('close');
+$('.intro h3').on('click', function() {
+	var $instructions = $(this).next('p');
+	if ($instructions.is(':visible')) {
+		$instructions.slideUp(200);
+		$(this).addClass('open')
+	} else { 
+		$instructions.slideDown(200);
+		$(this).removeClass('open');
+	} //End if else statement
+}); //End on click
+
 
 //Use button click to add tasks
 $('.submit-btn').on('click', function (event){
@@ -13,10 +26,19 @@ $('.submit-btn').on('click', function (event){
 		$('.warning').fadeIn(300);
 	} else { 
 		$('.warning').fadeOut(300);
-		$('li:first').before("<li>"+$newTask+"</li>");
+		$('<li class="tasks">'+$newTask+'</li>').prependTo('ul#sortable')
+		.css({
+			opacity : 0,
+			marginTop : "-10px"
+		})
+		.animate({
+			opacity : 1,
+			marginTop : 0
+		}, 300);
+
 		$('input:text').val('')
 	} //End if statement
-}); //End on click
+}); //End on click 
 
 
 //Sub Feature of Adding tasks by pressing enter
@@ -28,22 +50,36 @@ $('.input-field').on('keydown', function (event) {
 	} //End if statement
 }); // End on keydown
 
+
 //Make the Task List Sortable
 $('#sortable').sortable({axis : "y"});
 
 
-var updatedItem;
-$('#sortable li').on('dblclick', function (){
-	$(this).toggleClass('listclick');
-	updatedItem = prompt('Update Item');
-	$(this).html("<li>"+updatedItem+"</li>");
-
-});// End jQuery selection for updateItem function
-
+//Feature to edit added tasks/items
+// var updatedItem;
+// $('#sortable').on('dblclick', 'li', function (){
+// 	updatedItem = prompt('Update Item');
+// 	$(this).html('<li class="tasks">'+updatedItem+'</li>');
+// });// End jQuery selection for updateItem function
 
 
-//Selectable
 
+//Make it possible to complete tasks
+$('#sortable').on('click', 'li', function(){
+	$(this).toggleClass('selected');
+
+}); //End Click
+
+//Feature to delete selected tasks
+$('.delete').on('click', function(event) {
+	event.preventDefault();
+	$('.selected').slideUp();
+});
+
+
+//Priority colors
+$('.tasks:lt(5)').addClass("hot");
+$('.tasks:lt(10)').addClass("medium");
 
 
 
