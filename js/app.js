32df94c78399b3782697f88695bb7ca160150d8b
n8kolenberg <nkolenberg@gmail.com>
2015-08-23 23:27:01 +0100

    Trying to refactor the code towards MVC model

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(document).ready( function() {
+// $(document).ready( function() {
 
 //Put focus on the input field right away
 $('input:text').focus();
@@ -6,137 +6,125 @@ $('input:text').focus();
 $('.warning').hide();
 
 
-//Add icons for completion and deletion
-var completeMark = '<span class="completeMark icon-ok-circled2" contenteditable="false"></span>';
-var deleteMark = '<span class="deleteMark icon-trash-empty" contenteditable="false"></span>';
+/* VIEW
+* We need an input validator that checks whether the user has typed in something
+* It should give the user a message to enter something if they haven't typed anything in
+*/
 
+function InputView() {
+	this.element = $('.input-field');
+	this.button = $('.btn-warning');
+	this.button.click(this.validateInput.bind(this));
+	var self = this.validateInput.bind(this);
+	this.element.keydown(this.validateInput.bind(this));
+}
 
-
-//Sub Feature of Adding tasks by pressing enter
-$('.input-field').on('keydown', function (event) {
-	if(event.which === 13) {
-		event.preventDefault();
-		var $newTask = $('input:text').val();
-		if(!$.trim($newTask)) { 
-			$('.warning').fadeIn(300);
-		} else { 
-			$('.warning').fadeOut(300);
-			$('<li class="tasks" contenteditable="true">'+ completeMark + $newTask + deleteMark + '</li>').prependTo('#sortable')
-			.css({
-				opacity : 0,
-				marginTop : "-10px"
-			})
-			.animate({
-				opacity : 1,
-				marginTop : 0
-			}, 300);
-			$('input:text').val('').focus();
-		} //End if statement to determine whether to show warning
-	} //End if statement to determine whether Enter has been pressed
-}); // End on keydown
-
-
-
-//Complete the tasks
-$('#sortable').on('click', '.completeMark', function(event){
-	event.preventDefault();
-	if($(this).hasClass('icon-ok-circled2')) {
-		$(this).removeClass('icon-ok-circled2');
-		$(this).addClass('icon-ok-circled');
-		$(this).parent().css({
-			'text-decoration' : 'line-through',
-			'color' : 'rgba(44, 62, 80, 0.5)'
-		}); //End .css
+InputView.prototype.validateInput = function(event) {
+	var inputVal = $('.input-field').val();
+	if($.trim(inputVal) === "") {
+		$('.warning').fadeIn(300);
 	} else {
-		$(this).removeClass('icon-ok-circled');
-		$(this).addClass('icon-ok-circled2');
-		$(this).parent().css({
-			'text-decoration' : 'none',
-			'color' : 'rgba(44, 62, 80, 1)'
-		}); //End .css
-	} //End if statement
-});//End click
+		$('.warning').fadeOut(300);
+		listView.addListItem(inputVal);
+	}
+}; // End validateInput function
 
-//Delete the tasks
-$('#sortable').on('click', '.deleteMark', function(event){
-	event.preventDefault();
-		$(this).removeClass('icon-trash-empty');
-		$(this).addClass('icon-trash-1');
-		$(this).parent().attr("contenteditable", "false").fadeOut(1000).remove(100000);
-		// $(this).parent().remove(100000);
-});//End click
+var inputView = new InputView();
 
+//----------------------------------
+//----------------------------------
 
-//Feature to delete all tasks
-$('form').submit(function(event) {
-	event.preventDefault();
-	$('#sortable').children().fadeOut(500);
-	$('#sortable').children().remove(1000);
-}); //End submit function
+function ListView() {
+	this.element = $('#sortable');
+	this.element.on('click', '.icon-trash-empty', this.deleteListItem.bind(this));
+	this.element.on('click', '.icon-ok-circled2', this.checkListItem.bind(this))
+}
 
+ListView.prototype.addListItem = function(value) {
+	//Add icons for completion and deletion
+	var completeMark = '<span class="completeMark icon-ok-circled2" contenteditable="false"></span>';
+	var deleteMark = '<span class="deleteMark icon-trash-empty" contenteditable="false"></span>';
+	var listItem = "<li class='tasks' contenteditable='true'>";
+	listItem += completeMark;
+	listItem += value;
+	listItem += deleteMark + "</li>";
+	$('#sortable').append(listItem)
+	.css({
+		opacity : 0,
+		marginTop : "-10px"
+	})
+	.animate({
+		opacity : 1,
+		marginTop : 0
+	}, 300); //End append with animation
+	$('input:text').val('').focus();
+}
 
+ListView.prototype.checkListItem = function(event) {
+	$(event.target).closest('span').toggleClass('icon-ok-circled2').toggleClass('icon-ok-circled')
+		.closest('li')
+		.toggleClass('checked');
+}
 
+ListView.prototype.deleteListItem = function() {
+	$(event.target).closest('li').attr("contenteditable", "false").fadeOut('fast').remove(10000);
+}
 
-/*Other Tests I've tried throughout the assignment
-============================================================
-============================================================*/
-
-// //Make instructions appear and disappear
-// $('.intro h3').addClass('close');
-// $('.intro h3').on('click', function() {
-// 	var $instructions = $(this).next('p');
-// 	if ($instructions.is(':visible')) {
-// 		$instructions.slideUp(200);
-// 		$(this).addClass('open')
-// 	} else { 
-// 		$instructions.slideDown(200);
-// 		$(this).removeClass('open');
-// 	} //End if else statement
-// }); //End on click
-
-//Make the Task List Sortable
-// $(function() {
-// 	$('#sortable').sortable({
-// 		axis : "y",
-// 		cancel : ':input, button, [contenteditable]'
-// 	});
-// })// End sortable function
-
-
-//Add contenteditable = true via a dblclick handler
-// $(function(){
-//  $('#sortable').on('dblclick', 'li', function(){
-//    $(this).attr('contenteditable', 'true');
-//  });
-// });
+var listView = new ListView();
 
 
 
-//Feature to add priority colors to tasks based on position
-// function prioritize() {
-// $('#sortable').children().filter(function(index) {
-// 	console.log(index);
-// 	if (index >= 6) {
-// 		$(this).addClass("chill");
-// 	} else if (index > 2 && index < 6) {
-// 		$(this).addClass("medium");
-// 	} else {
-// 		$(this).addClass("hot");
-// 	}
-// 	return false;
-	// }); //End filter
-// } //End prioritize function
+//Controller
+
+// var completeMark = '<span class="completeMark icon-ok-circled2" contenteditable="false"></span>';
+// var deleteMark = '<span class="deleteMark icon-trash-empty" contenteditable="false"></span>';
+
+// //Sub Feature of Adding tasks by pressing enter
+// $('.input-field').on('keydown', function (event) {
+// 	if(event.which === 13) {
+// 		event.preventDefault();
+// 		var $newTask = $('input:text').val();
+// 		if(!$.trim($newTask)) { 
+// 			$('.warning').fadeIn(300);
+// 		} else { 
+// 			$('.warning').fadeOut(300);
+// 			$('<li class="tasks" contenteditable="true">'+ completeMark + $newTask + deleteMark + '</li>').prependTo('#sortable')
+// 			.css({
+// 				opacity : 0,
+// 				marginTop : "-10px"
+// 			})
+// 			.animate({
+// 				opacity : 1,
+// 				marginTop : 0
+// 			}, 300);
+// 			$('input:text').val('').focus();
+// 		} //End if statement to determine whether to show warning
+// 	} //End if statement to determine whether Enter has been pressed
+// }); // End on keydown
 
 
-// Wanted to try to preload images using jQuery
-// var bgImage = new Image();
-// bgImage.src = "img/notebook.jpeg";
-// bgImage.onload();
-// // 	function(){
-// // 	$('.wrapper').css({
-// // 		backgroundImage : url('../img/notebook.jpeg'),
-// // 		backgroundSize : 'cover'
-// // 	});
-// // });//End onload function
 
-}); //End ready
+// //Complete the tasks
+// $('#sortable').on('click', '.completeMark', function(event){
+// 		$(event.target).closest('span').toggleClass('icon-ok-circled2').toggleClass('icon-ok-circled')
+// 		.closest('li')
+// 		.toggleClass('checked');
+// });//End click
+
+// //Delete the tasks
+// $('#sortable').on('click', '.deleteMark', function(event){
+// 	event.preventDefault();
+// 		$(this).removeClass('icon-trash-empty');
+// 		$(this).addClass('icon-trash-1');
+// 		$(this).parent().attr("contenteditable", "false").fadeOut(1000).remove(100000);
+// });//End click
+
+
+// //Feature to delete all tasks
+// $('form').submit(function(event) {
+// 	event.preventDefault();
+// 	$('#sortable').children().fadeOut(500);
+// 	$('#sortable').children().remove(1000);
+// }); //End submit function
+
+// }); //End ready
